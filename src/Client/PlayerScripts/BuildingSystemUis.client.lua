2c999d0705638e25dc1fa1ab7bf07e9ac1d1a023
CluelessDev <cluelessdevmail@gmail.com>
2021-05-22 21:04:03 -0500

    Added Local player variable

--- src/Client/PlayerScripts/BuildingSystemUis.client.lua
+++ src/Client/PlayerScripts/BuildingSystemUis.client.lua
@@ -4,8 +4,8 @@ local RunService = game:GetService('RunService')
 local Players = game:GetService('Players')
 
 -- Data
-local Mouse = Players.LocalPlayer:GetMouse()
-print(Mouse)
+local player = Players.LocalPlayer
+local mouse = Players.LocalPlayer:GetMouse()
 
 --[[RunService.Heartbeat:Connect(function()
     print(Mouse.Target) 
@@ -19,16 +19,17 @@ local function StartBuildMode()
     p.Size = Vector3.new(10, 2, 10)
     p.BrickColor = BrickColor.new("Really red")
     
-    Mouse.TargetFilter = p
+    mouse.TargetFilter = p
 
     RunService.Heartbeat:Connect(function()
-        p.Position = Mouse.Target.Position + Vector3.new(0, 1, 0)
+        p.Position = mouse.Target.Position + Vector3.new(0, 1, 0)
     end)
 end
 
 
 UserInputService.InputBegan:Connect(function(anInput)
     if anInput.KeyCode == Enum.KeyCode.E then
+        
         StartBuildMode()
     end
 end)
