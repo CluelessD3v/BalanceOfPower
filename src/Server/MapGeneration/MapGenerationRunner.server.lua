447ef79969903dcd68a88d6824d9f0c043a87c0a
CluelessDev <cluelessdevmail@gmail.com>
2021-07-20 10:21:53 -0500

    Test: Implemented weighted drop rate system

--- src/Server/MapGeneration/MapGenerationRunner.server.lua
+++ src/Server/MapGeneration/MapGenerationRunner.server.lua
@@ -3,9 +3,13 @@ local CollectionService = game:GetService('CollectionService')
 local ReplicatedStorage = game:GetService('ReplicatedStorage')
 local ServerStorage = game:GetService('ServerStorage')
 
+
+local Utilities = ReplicatedStorage.Utilities
+
 -- Modules
 local MapClass = require(ServerStorage.Systems.MapEntity)
 local MapGenHelperLib = require(ServerStorage.Systems.MapGenHelperLib)
+local GetWeightedDrop = require(Utilities.GetWeightedDrop)
 
 -- Mapping MapGenerationConfig values to the map gen tbable
 
@@ -21,9 +25,57 @@ Map:UpdateTilesFromTag("Lowland",
     Properties = {},
     Tags = {"Iron"},
     Attributes = {
-        Threshold = .3
+        Threshold = .1
     },
 })
 
-MapGenHelperLib.SetTerrainElevation(Map)
+
+local Iron = {
+    {
+        Ammount = "Small",
+        Weight = 30
+    },
+
+    {
+        Ammount = "Medium",
+        Weight = 50
+    },
+    
+    {
+        Ammount = "Big",
+        Weight = 5
+    },
+}
+
+local sum = 0
+for _, entry in pairs(Iron) do
+    sum = sum + entry.Weight
+end
+
+
+
+local resource = nil
+
+
+local function getResource()
+    local randI = math.random(sum)
+    for _, entry in pairs(Iron) do
+        if randI <= entry.Weight then
+            return entry.Ammount     
+        else
+            randI = randI - entry.Weight   
+        end
+    end
+end
+
+
+for _, tile in ipairs(CollectionService:GetTagged("Iron")) do
+    local ammount = GetWeightedDrop(Iron)
+    print(ammount)
+    tile:SetAttribute("ResourceAmmount", ammount)
+end
+
+--MapGenHelperLib.SetTerrainElevation(Map)
+
+
 Map.Debug.FilterTiles.Whitelist(Map, {"Iron"})
---
+++ src/Shared/Utilities/GetWeightedDrop.lua
@@ -0,0 +1,24 @@
+-- table MUST be a dictionary and have a weight entry!
+
+local function CalculateMaxWeight(aTable)
+    local result = 0
+    for _, entry in pairs(aTable) do
+        result = result + entry.Weight
+    end
+
+    return result
+end
+
+return function(aWeightedTable)
+    local MaxWeight = CalculateMaxWeight(aWeightedTable)
+    
+    local randI = math.random(MaxWeight)
+    
+    for _, entry in pairs(aWeightedTable) do
+        if randI <= entry.Weight then
+            return entry.Ammount     
+        else
+            randI = randI - entry.Weight   
+        end
+    end 
+end
