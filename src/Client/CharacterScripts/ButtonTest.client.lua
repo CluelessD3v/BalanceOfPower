14caa54d1c258fb7174626a4220c0ecc35ef9a56
CluelessDev <cluelessdevmail@gmail.com>
2021-07-29 11:38:01 -0500

    Test: Tested Gui logic in starter character scripts

---
+++ src/Client/CharacterScripts/ButtonTest.client.lua
@@ -0,0 +1,8 @@
+local Players = game:GetService('Players')
+
+local localPlayer = Players.LocalPlayer
+local Gui = localPlayer.PlayerGui
+
+local button = Gui.PanelsGui.PanelButtons.ImageButton
+
+print(button)
